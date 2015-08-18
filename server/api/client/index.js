52b3be8f6267bde3be915a8d90322214168d62f6
benjipott <pott.benjamin@gmail.com>
2015-08-18 09:20:47 +0200

    secure api endpoint

--- server/api/client/index.js
+++ server/api/client/index.js
@@ -2,14 +2,14 @@
 
 var express = require('express');
 var controller = require('./client.controller');
-
+var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/:id', auth.hasRole('admin'), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
