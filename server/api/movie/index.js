5455d5e5661253e0266607e29e55446b1d933a01
benjipott <pott.benjamin@gmail.com>
2015-08-18 16:51:47 +0200

    remove access to all movies on api

--- server/api/movie/index.js
+++ server/api/movie/index.js
@@ -6,7 +6,7 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', auth.hasRole('admin'), controller.create);
 router.put('/:id', auth.hasRole('admin'), controller.update);
