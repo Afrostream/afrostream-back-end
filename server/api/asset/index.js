c192a45ada1293027f8478c7aff160f6040d1881
benjipott <pott.benjamin@gmail.com>
2015-08-26 09:05:50 +0200

    add proxy service digibos

--- server/api/asset/index.js
+++ server/api/asset/index.js
@@ -3,11 +3,14 @@
 var express = require('express');
 var controller = require('./asset.controller');
 var auth = require('../../auth/auth.service');
+var config = require('../../config/environment');
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.get('/:id/:token', auth.isAuthenticated(), controller.showToken);
+router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/:id', auth.hasRole('admin'), controller.show);
+if (config.digibos.useToken) {
+  router.get('/:id/:token/*', auth.isAuthenticated(), controller.proxify);
+}
 router.post('/', auth.hasRole('admin'), controller.create);
 router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', auth.hasRole('admin'), controller.update);
