58f437765f8d6cfa080abc4711d88c129864a69b
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-10-12 15:46:20 +0200

    refactoring: we only allow specific pages, instead of returning the home (avoiding socket.io/* mass downloads)

--- server/routes.js
+++ server/routes.js
@@ -41,14 +41,12 @@ module.exports = function (app) {
       res.sendFile(path.resolve(app.get('docPath') + '/index.html'));
     });
 
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-    .get(errors[404]);
-
-
   // All other routes should redirect to the index.html
-  app.route('/*')
+  app.route(/\/(categorys|licensors|movies|seasons|episodes|videos|languages|images|users|plans|subscriptions|clients|actors)/)
     .get(function (req, res) {
       res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
+
+  // undefined routes should return a 404
+  app.route('.*').get(errors[404]);
 };
