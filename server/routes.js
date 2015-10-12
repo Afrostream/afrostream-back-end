6dc9bc42da245fc172eedc54d0397444515ef838
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-10-12 15:52:42 +0200

    bugfix: adding settings & logout in allowed routes

--- server/routes.js
+++ server/routes.js
@@ -42,7 +42,7 @@ module.exports = function (app) {
     });
 
   // All other routes should redirect to the index.html
-  app.route(/\/(categorys|licensors|movies|seasons|episodes|videos|languages|images|users|plans|subscriptions|clients|actors)/)
+  app.route(/\/(categorys|licensors|movies|seasons|episodes|videos|languages|images|users|plans|subscriptions|clients|actors|settings|logout)/)
     .get(function (req, res) {
       res.set('Cache-Control', 'public, max-age=0');
       res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
