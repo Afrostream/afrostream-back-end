6ef1ac4244a78f19e8a56193d294d12e366ac787
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-10-25 16:15:30 +0100

    hotfix: 'sort' field on episode is <=> to episodeNumber

--- client/app/episodes/episodes.controller.js
+++ client/app/episodes/episodes.controller.js
@@ -2,6 +2,12 @@
 
 angular.module('afrostreamAdminApp')
   .controller('EpisodesCtrl', function ($scope, Season, Video) {
+    $scope.$watch('item.episodeNumber', function() {
+      if ($scope.item) {
+        $scope.item.sort = $scope.item.episodeNumber;
+      }
+    }, true);
+
     $scope.loadSeasons = function (query) {
       var p = Season.query({query: query}).$promise;
       p.then(function (response) {
