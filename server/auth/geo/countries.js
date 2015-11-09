474212a5c1fdfad2e34325aa43abebad53b01630
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-09 19:46:38 +0100

    feature: GET /auth/geo/

---
+++ server/auth/geo/countries.js
@@ -0,0 +1,519 @@
+'use strict';
+
+module.exports.authorizations = {
+  "A1": false, // Anonymous Proxy
+  "A2": false, // Satellite Provider
+  "O1": false, // Other Country
+  "AD": false, // Andorra
+  "AE": false, // United Arab Emirates
+  "AF": false, // Afghanistan
+  "AG": false, // Antigua and Barbuda
+  "AI": false, // Anguilla
+  "AL": false, // Albania
+  "AM": false, // Armenia
+  "AO": false, // Angola
+  "AP": false, // Asia/Pacific Region
+  "AQ": false, // Antarctica
+  "AR": false, // Argentina
+  "AS": false, // American Samoa
+  "AT": false, // Austria
+  "AU": false, // Australia
+  "AW": false, // Aruba
+  "AX": false, // Aland Islands
+  "AZ": false, // Azerbaijan
+  "BA": false, // Bosnia and Herzegovina
+  "BB": false, // Barbados
+  "BD": false, // Bangladesh
+  "BE": true, // Belgium
+  "BF": false, // Burkina Faso
+  "BG": false, // Bulgaria
+  "BH": false, // Bahrain
+  "BI": false, // Burundi
+  "BJ": false, // Benin
+  "BL": false, // Saint Bartelemey
+  "BM": false, // Bermuda
+  "BN": false, // Brunei Darussalam
+  "BO": false, // Bolivia
+  "BQ": false, // Bonaire, Saint Eustatius and Saba
+  "BR": false, // Brazil
+  "BS": false, // Bahamas
+  "BT": false, // Bhutan
+  "BV": false, // Bouvet Island
+  "BW": false, // Botswana
+  "BY": false, // Belarus
+  "BZ": false, // Belize
+  "CA": false, // Canada
+  "CC": false, // Cocos (Keeling) Islands
+  "CD": false, // Congo, The Democratic Republic of the
+  "CF": false, // Central African Republic
+  "CG": false, // Congo
+  "CH": false, // Switzerland
+  "CI": true, // Cote d'Ivoire
+  "CK": false, // Cook Islands
+  "CL": false, // Chile
+  "CM": false, // Cameroon
+  "CN": false, // China
+  "CO": false, // Colombia
+  "CR": false, // Costa Rica
+  "CU": false, // Cuba
+  "CV": false, // Cape Verde
+  "CW": false, // Curacao
+  "CX": false, // Christmas Island
+  "CY": false, // Cyprus
+  "CZ": false, // Czech Republic
+  "DE": false, // Germany
+  "DJ": false, // Djibouti
+  "DK": false, // Denmark
+  "DM": false, // Dominica
+  "DO": false, // Dominican Republic
+  "DZ": false, // Algeria
+  "EC": false, // Ecuador
+  "EE": false, // Estonia
+  "EG": false, // Egypt
+  "EH": false, // Western Sahara
+  "ER": false, // Eritrea
+  "ES": false, // Spain
+  "ET": false, // Ethiopia
+  "EU": false, // Europe
+  "FI": false, // Finland
+  "FJ": false, // Fiji
+  "FK": false, // Falkland Islands (Malvinas)
+  "FM": false, // Micronesia, Federated States of
+  "FO": false, // Faroe Islands
+  "FR": true, // France
+  "GA": false, // Gabon
+  "GB": false, // United Kingdom
+  "GD": false, // Grenada
+  "GE": false, // Georgia
+  "GF": false, // French Guiana
+  "GG": false, // Guernsey
+  "GH": false, // Ghana
+  "GI": false, // Gibraltar
+  "GL": false, // Greenland
+  "GM": false, // Gambia
+  "GN": false, // Guinea
+  "GP": true, // Guadeloupe
+  "GQ": false, // Equatorial Guinea
+  "GR": false, // Greece
+  "GS": false, // South Georgia and the South Sandwich Islands
+  "GT": false, // Guatemala
+  "GU": false, // Guam
+  "GW": false, // Guinea-Bissau
+  "GY": false, // Guyana
+  "HK": false, // Hong Kong
+  "HM": false, // Heard Island and McDonald Islands
+  "HN": false, // Honduras
+  "HR": false, // Croatia
+  "HT": false, // Haiti
+  "HU": false, // Hungary
+  "ID": false, // Indonesia
+  "IE": false, // Ireland
+  "IL": false, // Israel
+  "IM": false, // Isle of Man
+  "IN": false, // India
+  "IO": false, // British Indian Ocean Territory
+  "IQ": false, // Iraq
+  "IR": false, // Iran, Islamic Republic of
+  "IS": false, // Iceland
+  "IT": false, // Italy
+  "JE": false, // Jersey
+  "JM": false, // Jamaica
+  "JO": false, // Jordan
+  "JP": false, // Japan
+  "KE": false, // Kenya
+  "KG": false, // Kyrgyzstan
+  "KH": false, // Cambodia
+  "KI": false, // Kiribati
+  "KM": false, // Comoros
+  "KN": false, // Saint Kitts and Nevis
+  "KP": false, // Korea, Democratic People's Republic of
+  "KR": false, // Korea, Republic of
+  "KW": false, // Kuwait
+  "KY": false, // Cayman Islands
+  "KZ": false, // Kazakhstan
+  "LA": false, // Lao People's Democratic Republic
+  "LB": false, // Lebanon
+  "LC": false, // Saint Lucia
+  "LI": false, // Liechtenstein
+  "LK": false, // Sri Lanka
+  "LR": false, // Liberia
+  "LS": false, // Lesotho
+  "LT": false, // Lithuania
+  "LU": true, // Luxembourg
+  "LV": false, // Latvia
+  "LY": false, // Libyan Arab Jamahiriya
+  "MA": false, // Morocco
+  "MC": false, // Monaco
+  "MD": false, // Moldova, Republic of
+  "ME": false, // Montenegro
+  "MF": true, // Saint Martin
+  "MG": false, // Madagascar
+  "MH": false, // Marshall Islands
+  "MK": false, // Macedonia
+  "ML": false, // Mali
+  "MM": false, // Myanmar
+  "MN": false, // Mongolia
+  "MO": false, // Macao
+  "MP": false, // Northern Mariana Islands
+  "MQ": true, // Martinique
+  "MR": false, // Mauritania
+  "MS": false, // Montserrat
+  "MT": false, // Malta
+  "MU": false, // Mauritius
+  "MV": false, // Maldives
+  "MW": false, // Malawi
+  "MX": false, // Mexico
+  "MY": false, // Malaysia
+  "MZ": false, // Mozambique
+  "NA": false, // Namibia
+  "NC": false, // New Caledonia
+  "NE": false, // Niger
+  "NF": false, // Norfolk Island
+  "NG": false, // Nigeria
+  "NI": false, // Nicaragua
+  "NL": false, // Netherlands
+  "NO": false, // Norway
+  "NP": false, // Nepal
+  "NR": false, // Nauru
+  "NU": false, // Niue
+  "NZ": false, // New Zealand
+  "OM": false, // Oman
+  "PA": false, // Panama
+  "PE": false, // Peru
+  "PF": true, // French Polynesia
+  "PG": false, // Papua New Guinea
+  "PH": false, // Philippines
+  "PK": false, // Pakistan
+  "PL": false, // Poland
+  "PM": false, // Saint Pierre and Miquelon
+  "PN": false, // Pitcairn
+  "PR": false, // Puerto Rico
+  "PS": false, // Palestinian Territory
+  "PT": false, // Portugal
+  "PW": false, // Palau
+  "PY": false, // Paraguay
+  "QA": false, // Qatar
+  "RE": true, // Reunion
+  "RO": false, // Romania
+  "RS": false, // Serbia
+  "RU": false, // Russian Federation
+  "RW": false, // Rwanda
+  "SA": false, // Saudi Arabia
+  "SB": false, // Solomon Islands
+  "SC": false, // Seychelles
+  "SD": false, // Sudan
+  "SE": false, // Sweden
+  "SG": false, // Singapore
+  "SH": false, // Saint Helena
+  "SI": false, // Slovenia
+  "SJ": false, // Svalbard and Jan Mayen
+  "SK": false, // Slovakia
+  "SL": false, // Sierra Leone
+  "SM": false, // San Marino
+  "SN": true, // Senegal
+  "SO": false, // Somalia
+  "SR": false, // Suriname
+  "SS": false, // South Sudan
+  "ST": false, // Sao Tome and Principe
+  "SV": false, // El Salvador
+  "SX": false, // Sint Maarten
+  "SY": false, // Syrian Arab Republic
+  "SZ": false, // Swaziland
+  "TC": false, // Turks and Caicos Islands
+  "TD": false, // Chad
+  "TF": false, // French Southern Territories
+  "TG": false, // Togo
+  "TH": false, // Thailand
+  "TJ": false, // Tajikistan
+  "TK": false, // Tokelau
+  "TL": false, // Timor-Leste
+  "TM": false, // Turkmenistan
+  "TN": false, // Tunisia
+  "TO": false, // Tonga
+  "TR": false, // Turkey
+  "TT": false, // Trinidad and Tobago
+  "TV": false, // Tuvalu
+  "TW": false, // Taiwan
+  "TZ": false, // Tanzania, United Republic of
+  "UA": false, // Ukraine
+  "UG": false, // Uganda
+  "UM": false, // United States Minor Outlying Islands
+  "US": false, // United States
+  "UY": false, // Uruguay
+  "UZ": false, // Uzbekistan
+  "VA": false, // Holy See (Vatican City State)
+  "VC": false, // Saint Vincent and the Grenadines
+  "VE": false, // Venezuela
+  "VG": false, // Virgin Islands, British
+  "VI": false, // Virgin Islands, U.S.
+  "VN": false, // Vietnam
+  "VU": false, // Vanuatu
+  "WF": false, // Wallis and Futuna
+  "WS": false, // Samoa
+  "YE": false, // Yemen
+  "YT": false, // Mayotte
+  "ZA": false, // South Africa
+  "ZM": false, // Zambia
+  "ZW": false, // Zimbabwe"
+  "": true,    // UNKNOWN => full access.
+  "--": true   // UNKNOWN => full access.
+};
+
+module.exports.names = {
+  "A1": "Anonymous Proxy",
+  "A2": "Satellite Provider",
+  "O1": "Other Country",
+  "AD": "Andorra",
+  "AE": "United Arab Emirates",
+  "AF": "Afghanistan",
+  "AG": "Antigua and Barbuda",
+  "AI": "Anguilla",
+  "AL": "Albania",
+  "AM": "Armenia",
+  "AO": "Angola",
+  "AP": "Asia/Pacific Region",
+  "AQ": "Antarctica",
+  "AR": "Argentina",
+  "AS": "American Samoa",
+  "AT": "Austria",
+  "AU": "Australia",
+  "AW": "Aruba",
+  "AX": "Aland Islands",
+  "AZ": "Azerbaijan",
+  "BA": "Bosnia and Herzegovina",
+  "BB": "Barbados",
+  "BD": "Bangladesh",
+  "BE": "Belgium",
+  "BF": "Burkina Faso",
+  "BG": "Bulgaria",
+  "BH": "Bahrain",
+  "BI": "Burundi",
+  "BJ": "Benin",
+  "BL": "Saint Bartelemey",
+  "BM": "Bermuda",
+  "BN": "Brunei Darussalam",
+  "BO": "Bolivia",
+  "BQ": "Bonaire, Saint Eustatius and Saba",
+  "BR": "Brazil",
+  "BS": "Bahamas",
+  "BT": "Bhutan",
+  "BV": "Bouvet Island",
+  "BW": "Botswana",
+  "BY": "Belarus",
+  "BZ": "Belize",
+  "CA": "Canada",
+  "CC": "Cocos (Keeling) Islands",
+  "CD": "Congo, The Democratic Republic of the",
+  "CF": "Central African Republic",
+  "CG": "Congo",
+  "CH": "Switzerland",
+  "CI": "Cote d'Ivoire",
+  "CK": "Cook Islands",
+  "CL": "Chile",
+  "CM": "Cameroon",
+  "CN": "China",
+  "CO": "Colombia",
+  "CR": "Costa Rica",
+  "CU": "Cuba",
+  "CV": "Cape Verde",
+  "CW": "Curacao",
+  "CX": "Christmas Island",
+  "CY": "Cyprus",
+  "CZ": "Czech Republic",
+  "DE": "Germany",
+  "DJ": "Djibouti",
+  "DK": "Denmark",
+  "DM": "Dominica",
+  "DO": "Dominican Republic",
+  "DZ": "Algeria",
+  "EC": "Ecuador",
+  "EE": "Estonia",
+  "EG": "Egypt",
+  "EH": "Western Sahara",
+  "ER": "Eritrea",
+  "ES": "Spain",
+  "ET": "Ethiopia",
+  "EU": "Europe",
+  "FI": "Finland",
+  "FJ": "Fiji",
+  "FK": "Falkland Islands (Malvinas)",
+  "FM": "Micronesia, Federated States of",
+  "FO": "Faroe Islands",
+  "FR": "France",
+  "GA": "Gabon",
+  "GB": "United Kingdom",
+  "GD": "Grenada",
+  "GE": "Georgia",
+  "GF": "French Guiana",
+  "GG": "Guernsey",
+  "GH": "Ghana",
+  "GI": "Gibraltar",
+  "GL": "Greenland",
+  "GM": "Gambia",
+  "GN": "Guinea",
+  "GP": "Guadeloupe",
+  "GQ": "Equatorial Guinea",
+  "GR": "Greece",
+  "GS": "South Georgia and the South Sandwich Islands",
+  "GT": "Guatemala",
+  "GU": "Guam",
+  "GW": "Guinea-Bissau",
+  "GY": "Guyana",
+  "HK": "Hong Kong",
+  "HM": "Heard Island and McDonald Islands",
+  "HN": "Honduras",
+  "HR": "Croatia",
+  "HT": "Haiti",
+  "HU": "Hungary",
+  "ID": "Indonesia",
+  "IE": "Ireland",
+  "IL": "Israel",
+  "IM": "Isle of Man",
+  "IN": "India",
+  "IO": "British Indian Ocean Territory",
+  "IQ": "Iraq",
+  "IR": "Iran, Islamic Republic of",
+  "IS": "Iceland",
+  "IT": "Italy",
+  "JE": "Jersey",
+  "JM": "Jamaica",
+  "JO": "Jordan",
+  "JP": "Japan",
+  "KE": "Kenya",
+  "KG": "Kyrgyzstan",
+  "KH": "Cambodia",
+  "KI": "Kiribati",
+  "KM": "Comoros",
+  "KN": "Saint Kitts and Nevis",
+  "KP": "Korea, Democratic People's Republic of",
+  "KR": "Korea, Republic of",
+  "KW": "Kuwait",
+  "KY": "Cayman Islands",
+  "KZ": "Kazakhstan",
+  "LA": "Lao People's Democratic Republic",
+  "LB": "Lebanon",
+  "LC": "Saint Lucia",
+  "LI": "Liechtenstein",
+  "LK": "Sri Lanka",
+  "LR": "Liberia",
+  "LS": "Lesotho",
+  "LT": "Lithuania",
+  "LU": "Luxembourg",
+  "LV": "Latvia",
+  "LY": "Libyan Arab Jamahiriya",
+  "MA": "Morocco",
+  "MC": "Monaco",
+  "MD": "Moldova, Republic of",
+  "ME": "Montenegro",
+  "MF": "Saint Martin",
+  "MG": "Madagascar",
+  "MH": "Marshall Islands",
+  "MK": "Macedonia",
+  "ML": "Mali",
+  "MM": "Myanmar",
+  "MN": "Mongolia",
+  "MO": "Macao",
+  "MP": "Northern Mariana Islands",
+  "MQ": "Martinique",
+  "MR": "Mauritania",
+  "MS": "Montserrat",
+  "MT": "Malta",
+  "MU": "Mauritius",
+  "MV": "Maldives",
+  "MW": "Malawi",
+  "MX": "Mexico",
+  "MY": "Malaysia",
+  "MZ": "Mozambique",
+  "NA": "Namibia",
+  "NC": "New Caledonia",
+  "NE": "Niger",
+  "NF": "Norfolk Island",
+  "NG": "Nigeria",
+  "NI": "Nicaragua",
+  "NL": "Netherlands",
+  "NO": "Norway",
+  "NP": "Nepal",
+  "NR": "Nauru",
+  "NU": "Niue",
+  "NZ": "New Zealand",
+  "OM": "Oman",
+  "PA": "Panama",
+  "PE": "Peru",
+  "PF": "French Polynesia",
+  "PG": "Papua New Guinea",
+  "PH": "Philippines",
+  "PK": "Pakistan",
+  "PL": "Poland",
+  "PM": "Saint Pierre and Miquelon",
+  "PN": "Pitcairn",
+  "PR": "Puerto Rico",
+  "PS": "Palestinian Territory",
+  "PT": "Portugal",
+  "PW": "Palau",
+  "PY": "Paraguay",
+  "QA": "Qatar",
+  "RE": "Reunion",
+  "RO": "Romania",
+  "RS": "Serbia",
+  "RU": "Russian Federation",
+  "RW": "Rwanda",
+  "SA": "Saudi Arabia",
+  "SB": "Solomon Islands",
+  "SC": "Seychelles",
+  "SD": "Sudan",
+  "SE": "Sweden",
+  "SG": "Singapore",
+  "SH": "Saint Helena",
+  "SI": "Slovenia",
+  "SJ": "Svalbard and Jan Mayen",
+  "SK": "Slovakia",
+  "SL": "Sierra Leone",
+  "SM": "San Marino",
+  "SN": "Senegal",
+  "SO": "Somalia",
+  "SR": "Suriname",
+  "SS": "South Sudan",
+  "ST": "Sao Tome and Principe",
+  "SV": "El Salvador",
+  "SX": "Sint Maarten",
+  "SY": "Syrian Arab Republic",
+  "SZ": "Swaziland",
+  "TC": "Turks and Caicos Islands",
+  "TD": "Chad",
+  "TF": "French Southern Territories",
+  "TG": "Togo",
+  "TH": "Thailand",
+  "TJ": "Tajikistan",
+  "TK": "Tokelau",
+  "TL": "Timor-Leste",
+  "TM": "Turkmenistan",
+  "TN": "Tunisia",
+  "TO": "Tonga",
+  "TR": "Turkey",
+  "TT": "Trinidad and Tobago",
+  "TV": "Tuvalu",
+  "TW": "Taiwan",
+  "TZ": "Tanzania, United Republic of",
+  "UA": "Ukraine",
+  "UG": "Uganda",
+  "UM": "United States Minor Outlying Islands",
+  "US": "United States",
+  "UY": "Uruguay",
+  "UZ": "Uzbekistan",
+  "VA": "Holy See (Vatican City State)",
+  "VC": "Saint Vincent and the Grenadines",
+  "VE": "Venezuela",
+  "VG": "Virgin Islands, British",
+  "VI": "Virgin Islands, U.S.",
+  "VN": "Vietnam",
+  "VU": "Vanuatu",
+  "WF": "Wallis and Futuna",
+  "WS": "Samoa",
+  "YE": "Yemen",
+  "YT": "Mayotte",
+  "ZA": "South Africa",
+  "ZM": "Zambia",
+  "ZW": "Zimbabwe",
+  "": "unknown",
+  "--": "unknown"
+};
