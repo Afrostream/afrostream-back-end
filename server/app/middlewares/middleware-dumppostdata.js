c8250d15479660b6e39bc8c2c8388002f3556a84
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-09 11:24:18 +0100

    fix: do not dump users sensitive info into logentries.

--- server/app/middlewares/middleware-dumppostdata.js
+++ server/app/middlewares/middleware-dumppostdata.js
@@ -3,7 +3,12 @@
 module.exports = function (options) {
   return function (req, res, next) {
     if (req && req.body) {
-      console.log(req.url + ' postData =', req.body);
+      try {
+        // fix: do not dump users passwords into logentries.
+        var body = JSON.parse(JSON.stringify(req.body));
+        if (body.password) { body.password = 'xxxxxxxx'; }
+        console.log(req.url + ' postData =', body);
+      } catch (e) { }
     }
     next();
   };
