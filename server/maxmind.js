95038aea4103e15e531cd950dbc371535eb2bca1
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-10 10:43:23 +0100

    debug

--- server/maxmind.js
+++ server/maxmind.js
@@ -6,12 +6,14 @@ maxmind.init(__dirname + '/data/GeoIP.dat');
 maxmind.init(__dirname + '/data/GeoIPv6.dat');
 
 var getCountryCode = function (ip) {
+  console.log('maxmind: getCountryCode: ['+ip+']');
   var country;
   try {
     country = maxmind.getCountry(ip);
   } catch (e) {
     console.error('maxmind error ', e);
   }
+  console.log('maxmind: getCountryCode:', country);
   return (country && country.code) ? country.code : '';
 };
 
