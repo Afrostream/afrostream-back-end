e070905435b0c84f4a0dabbfca330d3dbd53ec09
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-24 17:17:36 +0100

    auth: staging: allowing cross domain requests for testing purposes

--- server/auth/oauth2/index.js
+++ server/auth/oauth2/index.js
@@ -4,6 +4,24 @@ var express = require('express');
 var oauth2 = require('./oauth2');
 var router = express.Router();
 
+// cross domain access to our api, staging only for tests
+if (process.env.NODE_ENV === 'staging') {
+  router.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    next();
+  });
+
+  router.use(function (req, res, next) {
+    if (req.method === 'OPTIONS') {
+      res.send();
+    } else {
+      next();
+    }
+  });
+}
+
 router.post('/', oauth2.login);
 router.post('/token', oauth2.token);
 router.post('/autorization', oauth2.authorization);
