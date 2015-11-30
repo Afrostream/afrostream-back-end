596d7615e42e0029c8367a10669ae34b32ce8d80
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-30 09:55:06 +0100

    drm: staging: remove fake account id

--- server/right/index.js
+++ server/right/index.js
@@ -5,21 +5,6 @@ var controller = require('./right.controller');
 
 var router = express.Router();
 
-if (process.env.NODE_ENV === 'staging') {
-  router.get('/user/:userId/asset/:assetId', function (req, res) {
-    res.json({
-      "accountingId": 'staging fake accounting id',
-      "profile": {
-        "rental" : {
-          "absoluteExpiration" : new Date(new Date().getTime() + 1000 * 3600* 24).toISOString(), // 1 day
-          "playDuration" : 1000 * 3600 * 12 // 12 hours
-        }
-      },
-      "message":"granted"
-    });
-  });
-} else {
-  router.get('/user/:userId/asset/:assetId', controller.drmtodayCallback);
-}
+router.get('/user/:userId/asset/:assetId', controller.drmtodayCallback);
 
 module.exports = router;
