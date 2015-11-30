c58108288d8013a8dcff8e0fe8a1f59d365cbb63
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-30 14:14:20 +0100

    database: chore: remove unused fields, should resolve #131 & resolve #132

---
+++ sql/issue-#131-#132-remove-videos-fields.sql
@@ -0,0 +1,5 @@
+-- remove columns
+ALTER TABLE "Videos" DROP COLUMN "assetId";
+ALTER TABLE "Videos" DROP COLUMN "captionId";
+ALTER TABLE "Videos" DROP COLUMN "movieId";
+ALTER TABLE "Videos" DROP COLUMN "episodeId";
