aa572f12acf92d44128f75c62d37b104f111f150
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-12-14 15:30:46 +0100

    bugfix: avoid dumping personnal info in the logs.

--- server/app/middlewares/middleware-dumppostdata.js
+++ server/app/middlewares/middleware-dumppostdata.js
@@ -7,6 +7,12 @@ module.exports = function (options) {
         // fix: do not dump users passwords into logentries.
         var body = JSON.parse(JSON.stringify(req.body));
         if (body.password) { body.password = 'xxxxxxxx'; }
+        if (body.number) {
+          body.number = '4111111111111111';
+          if (body.year) body.year = 2018;
+          if (body.month) body.month = 1;
+        }
+        if (body.cvv) { body.cvv = '123'; }
         console.log(req.url + ' postData =', body);
       } catch (e) { }
     }
