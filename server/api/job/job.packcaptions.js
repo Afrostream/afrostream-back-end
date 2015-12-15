e9cf9904e7ee93c1a1d303b498d2157b1dc7883b
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-12-15 10:18:07 +0100

    refactor: removing extractEncodingId(): video.encodingId now exists

--- server/api/job/job.packcaptions.js
+++ server/api/job/job.packcaptions.js
@@ -10,26 +10,6 @@ var Language = sqldb.Language;
 
 var createJob = require('./job.generic.js').create;
 
-// encodingId : FIXME: we shouldn't need to parse the video ism file...
-function extractEncodingId(video, callback) {
-  return Q()
-    .then(function () {
-      if (!video.sources || !video.sources.length) {
-        throw "cannot extrazct EncodingId, missing source";
-      }
-      // src ~= https://origin.cdn.afrostream.net/vod/24hourlovebis/d4eed726882a4be3.ism/d4eed726882a4be3.mpd
-      var matches = video.sources[0].src.match(/\/([0-9a-f]+)\.ism/);
-      if (!matches) {
-        throw "cannot extract EncodingId, incorect src " + video.sources[0].src;
-      }
-      var encodingId = matches[1];
-      if (!encodingId) {
-        throw "encodingId doesn't exist";
-      }
-      return encodingId;
-    })
-}
-
 var create = function (videoId) {
   return Q()
     .then(function readVideo() {
@@ -50,23 +30,17 @@ var create = function (videoId) {
       return video;
     })
     .then(function (video) {
-      return extractEncodingId(video)
-        .then(function (encodingId) {
-          var captions = video.captions.map(function (caption) {
-            return {
-              src: caption.src,
-              lang: caption.lang.get('ISO6392T')
-            };
-          });
-
-          var data = {
-            videoId: videoId,
-            encodingId: encodingId,
-            captions: captions
+      var data = {
+        videoId: videoId,
+        encodingId: video.encodingId,
+        captions: video.captions.map(function (caption) {
+          return {
+            src: caption.src,
+            lang: caption.lang.get('ISO6392T')
           };
-
-          return createJob('pack captions', data, undefined);
         })
+      };
+      return createJob('pack captions', data, undefined);
     });
 };
 
