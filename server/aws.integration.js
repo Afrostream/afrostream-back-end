c25829dc896f5d9b1f525953da63030f1095f7a7
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-12-16 10:03:50 +0100

    aws: helper functions to upload a buffer to a bucket

---
+++ server/aws.integration.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var assert = require('better-assert');
+
+var bootstrap = require('../tests/bootstrap.js');
+
+var fs = require('fs');
+
+var aws = require('./aws');
+
+describe('Send to bucket', function() {
+  var kitten;
+
+  // load kitten file
+  before(function () {
+    kitten = fs.readFileSync(__dirname + '/../tests/data/kitten.jpg');
+  });
+
+  it('should load in aws without errors', function(done) {
+    var bucket = aws.getBucket('afrostream-img');
+    aws.putBufferIntoBucket(bucket, kitten, 'image/jpeg', 'test/{env}/{date}/{rand}-kitten.jpg')
+      .then(function (data) {
+        assert(data.req.url.match(/kitten\.jpg/));
+        done();
+      })
+  });
+});
---
+++ server/aws.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var assert = require('better-assert');
+
+var crypto = require('crypto')
+  , path = require('path');
+
+var Q = require('q');
+
+var Knox = require('knox')
+  , moment = require('moment');
+
+var config = require('./config/environment');
+
+/**
+ * upload a buffer in amazon AWS.
+ *
+ * ex:
+ *   aws.putBufferIntoBucket(bucket, buffer, 'image/jpeg', '{env}/cats/{date}/{rand}-toto.jpg')
+ *
+ * @param bucket    aws bucket
+ * @param buffer    Buffer
+ * @param mimeType  string
+ * @param path      string
+ * @return Promise<Unknown>
+ */
+module.exports.putBufferIntoBucket = function (bucket, buffer, mimeType, path) {
+  assert(bucket);
+  assert(buffer instanceof Buffer);
+  assert(typeof mimeType === 'string');
+  assert(typeof path === 'string' && path);
+
+  // Generate date based folder prefix
+  var datePrefix = moment().format('YYYY[/]MM');
+  var key = crypto.randomBytes(10).toString('hex');
+
+  path = path
+    .replace('{env}', config.env)
+    .replace('{date}', datePrefix)
+    .replace('{rand}', key);
+
+  var headers = {
+    'Content-Length': buffer.length,
+    'Content-Type': mimeType,
+    'x-amz-acl': 'public-read'
+  };
+
+  return Q.ninvoke(bucket, 'putBuffer', buffer, path, headers);
+};
+
+/**
+ * return a bucket
+ * @param bucketName    string
+ * @return object (bucket aws)
+ */
+module.exports.getBucket = function (bucketName) {
+  var client = Knox.createClient({
+    key:    config.amazon.key,
+    secret: config.amazon.secret,
+    bucket: bucketName || config.amazon.s3Bucket,
+    region: config.amazon.region
+  });
+  return client;
+};
