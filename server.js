aace026c99a0c591c206095bd4e32bcb0a7cccb8
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-01-12 10:30:10 +0100

    cluster: backend is cluster friendly.

--- server.js
+++ server.js
@@ -2,11 +2,7 @@
 
 var cluster = require('express-cluster');
 
-// FIXME: we need to look at the code to see if
-//  back-end is "cluster" friendly
-// ex: @see express session & passport.
-// temporary disabling cluster of multiple workers
-var clusterConf = {count: 1 /*process.env.WEB_CONCURRENCY || 1 */, verbose: true};
+var clusterConf = { count: process.env.WEB_CONCURRENCY || 1, verbose: true};
 
 cluster(function (worker) {
   console.log('worker '+worker.id+' is up');
