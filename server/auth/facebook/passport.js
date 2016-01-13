b5638488e883b99ebcf968d9f8a6d6b2bd9fd387
benjipott <pott.benjamin@gmail.com>
2016-01-13 09:51:08 +0100

    sync user if already exist

--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -10,35 +10,51 @@ exports.setup = function (User, config) {
       profileFields: [
         'displayName',
         'emails'
-      ]
+      ],
+      passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
     },
-    function (accessToken, refreshToken, profile, done) {
-      User.find({
-          where: {
-            'facebook.id': profile.id
-          }
-        })
-        .then(function (user) {
-          if (user) {
-            return done(null, user);
-          }
-          user = User.build({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            role: 'user',
-            provider: 'facebook',
-            facebook: profile._json
-          });
-          user.save()
-            .then(function (user) {
+    function (req, accessToken, refreshToken, profile, done) {
+      // check if the user is already logged in
+      if (!req.user) {
+        User.find({
+            where: {
+              'facebook.id': profile.id
+            }
+          })
+          .then(function (user) {
+            if (user) {
               return done(null, user);
-            })
-            .catch(function (err) {
-              return done(err);
+            }
+            // if there is no user, create them
+            user = User.build({
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              role: 'user',
+              provider: 'facebook',
+              facebook: profile._json
             });
-        })
-        .catch(function (err) {
+            user.save()
+              .then(function (user) {
+                return done(null, user);
+              })
+              .catch(function (err) {
+                return done(err);
+              });
+          })
+          .catch(function (err) {
+            return done(err);
+          });
+      } else {
+        // user already exists and is logged in, we have to link accounts
+        var user = req.user; // pull the user out of the session
+        user.updateAttributes({
+            facebook: profile._json
+          })
+          .then(function (linkedUser) {
+            return done(null, linkedUser);
+          }).catch(function (err) {
           return done(err);
         });
+      }
     }));
 };
