df725b9e25d9e7cf2a7b7627cc95276281fc2ab9
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-01-14 15:49:00 +0100

    api: /auth/reset: opening the route !

--- server/auth/index.js
+++ server/auth/index.js
@@ -7,6 +7,8 @@ var User = require('../sqldb').User;
 var Client = require('../sqldb').Client;
 var AccessToken = require('../sqldb').AccessToken;
 
+var auth = rootRequire('/server/auth/auth.service');
+
 // Passport Configuration
 require('./local/passport').setup(User, config);
 require('./oauth2/passport').setup(Client, User, AccessToken, config);
@@ -24,5 +26,6 @@ else {
   router.use('/local', require('./local'));
 }
 router.use('/google', require('./google'));
+router.post('/reset', auth.isAuthenticated(), require('./auth.controller.js').reset);
 
 module.exports = router;
