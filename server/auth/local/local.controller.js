f464cd6607a20128d856d98c1d34094bfd6b43b2
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-01-19 15:30:09 +0100

    reactor: login strategy local

---
+++ server/auth/local/local.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Q = require('q');
+
+var passport = require('passport');
+var auth = require('../auth.service');
+
+var login = function (req, res) {
+  Q.ninvoke(passport, 'authenticate', 'local')
+    .then(function (data) {
+      var user = data[0], error = data[1];
+      if (error) {
+        throw error;
+      }
+      if (!user) {
+        throw 'Something went wrong, please try again.'
+      }
+      return auth.getOauth2UserToken(user);
+    })
+    .then(
+    function (token) {
+      res.json({token: token});
+    },
+    function (err) {
+      return res.status(401).json({message: String(err)});
+    });
+};
+
+module.exports.login = login;
