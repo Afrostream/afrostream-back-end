aa125b8b95ad9823fe56059a5073136c49fbbc01
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-01-25 10:43:20 +0100

    enhancement: improving res.cache() & adding res.noCache()

--- server/app/middlewares/middleware-cachehandler.js
+++ server/app/middlewares/middleware-cachehandler.js
@@ -2,16 +2,20 @@
 
 module.exports = function (options) {
   return function cacheHandler(req, res, next) {
+    res.noCache = function () {
+      res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+      res.set('Pragma', 'no-cache'); // http 1.0
+      res.set('Expires', '0'); // proxy
+    };
     res.isDynamic = function () {
       res.set('Cache-Control', 'public, max-age=0');
     };
     res.cache = function (duration) {
-      res.set('Cache-Control', 'public, max-age=' + (duration || 60));
+      res.set('Cache-Control', 'public, max-age=' + (duration || 60) + ', stale-while-revalidate=10');
     };
     res.isStatic = function () {
       res.set('Cache-Control', 'public, max-age=31536000');
     };
-    // all routes are dynamic by default
     res.isDynamic();
     next();
   };
