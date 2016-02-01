5885bce704ec483e6ab9c53b5c914e5181acbc3d
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-02-01 10:01:45 +0100

    sql: migrating data from "AccessTokens" to "Logs"

--- sql/issue-#190-users-logs.sql
+++ sql/issue-#190-users-logs.sql
@@ -9,7 +9,12 @@ CREATE TABLE "Logs"
   type character varying(32),
   "userId" integer,
   "clientId" uuid,
-  data json
+  data json,
+  CONSTRAINT "Logs_pkey" PRIMARY KEY (_id)
 ) WITH (
   OIDS=FALSE
 );
+
+-- migration of old AccessLogs data into Logs
+insert into "Logs" ("createdAt", "type", "userId", "clientId", "data")
+  select "created" as "createdAt", 'access_token' as "type", "userId", "clientId", json_build_object('token', "token", 'userIp', "userIp", 'userAgent', "userAgent") as "data" from "AccessTokens" order by "createdAt"
