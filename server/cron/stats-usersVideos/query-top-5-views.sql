f55e96afb416c72bca58eea7513781e8c597e3cd
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-03-02 15:13:02 +0100

    cron: some stats for #content channel

---
+++ server/cron/stats-usersVideos/query-top-5-views.sql
@@ -0,0 +1,8 @@
+select count("UsersVideos"."userId") as "nbUsers", "UsersVideos"."videoId", "Videos"."name" from "UsersVideos" 
+      inner join "Videos" ON "UsersVideos"."videoId" = "Videos"."_id" 
+      where
+	      "dateLastRead" > '{{dateLastReadFrom}}' and
+	      "dateLastRead" < '{{dateLastReadTo}}'
+      group by "UsersVideos"."videoId", "Videos"."name"
+      order by "nbUsers" desc
+      limit 10
