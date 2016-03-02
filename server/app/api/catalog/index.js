5a22fedfa07a3e87ccd5c9d8ad20582639863a45
benjipott <pott.benjamin@gmail.com>
2016-03-02 12:05:22 +0100

    implement sitemap

---
+++ server/app/api/catalog/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @api {get} /api/gatalog Request Catalog information
+ * @apiName GetCatalog
+ * @apiGroup Catalog
+ *
+ * @apiHeader Authorization Basic Access Authentication token.
+ * @apiHeader Content-Type (application/x-www-form-urlencoded, application/json, application/xml).
+ * @apiSuccess (Success 201) {text} Location URI of created Thing.
+ * @apiSuccess (Success 201) {text} body Thing id.
+ * @apiError {text} 401/Unauthorized.
+ * @apiError {text} 403/Forbidden Required field [...] omitted.
+ *
+ * @apiSuccess {Object} Movie/Season/Video/Epidode.
+ *    curl -i https://legacy-api.afrostream.tv/api/catalog
+ * @apiSampleRequest https://legacy-api.afrostream.tv/api/catalog
+ */
+
+var express = require('express');
+var controller = require('./catalog.controller.js');
+
+var router = express.Router();
+
+router.get('/', controller.index);
