a5848efd5599b1d0f1da8c7ef65e3189e62934cd
benjipott <pott.benjamin@gmail.com>
2016-03-03 15:42:04 +0100

    billing : mock cancel subscription

--- server/test/mock-billing-api.js
+++ server/test/mock-billing-api.js
@@ -127,6 +127,62 @@ nock(config.billings.url)
 
 nock(config.billings.url)
   .persist() // FIXME: we should call nock on demand
+  .put('/billings/api/subscriptions/mock-billing-api/cancel')
+  .query(true)
+  .reply(200, {
+    status: "done",
+    statusMessage: "success",
+    statusCode: 0,
+    response: {
+      subscriptions: [
+        {
+          subscriptionBillingUuid: "mock-billing-api",
+          subscriptionProviderUuid: "mock-billing-api",
+          isActive: "yes",
+          user: {
+            userBillingUuid: "mock-billing-api",
+            userReferenceUuid: "123456",
+            userProviderUuid: "mock-billing-api",
+            provider: {
+              providerName: "gocardless"
+            },
+            userOpts: {
+              email: "mock-billing-api",
+              firstName: "mock-billing-api",
+              lastName: "mock-billing-api"
+            }
+          },
+          provider: {
+            providerName: "gocardless"
+          },
+          creationDate: "2016-02-03 10:02:50.750197+00",
+          updatedDate: "2016-02-03 10:02:50.750197+00",
+          subStatus: "canceled",
+          subActivatedDate: "2015-08-31 23:00:00+00",
+          subCanceledDate: null,
+          subExpiresDate: null,
+          subPeriodStartedDate: "2015-08-31 23:00:00+00",
+          subPeriodEndsDate: "2016-08-31 23:00:00+00",
+          internalPlan: {
+            internalPlanUuid: "afrostreamambassadeurs3",
+            name: "Ambassadeurs",
+            description: "Ambassadeurs",
+            amount_in_cents: "5999",
+            currency: "EUR",
+            cycle: "once",
+            periodUnit: "year",
+            periodLength: "1",
+            internalPlanOpts: {
+              internalMaxScreens: "2"
+            }
+          }
+        }
+      ]
+    }
+  });
+
+nock(config.billings.url)
+  .persist() // FIXME: we should call nock on demand
   .get('/billings/api/internalplans/')
   .query({providerName: 'bachat', userReferenceUuid: /.*/})
   .reply(200, {
