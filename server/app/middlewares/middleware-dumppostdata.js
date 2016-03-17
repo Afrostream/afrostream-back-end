04f48939f70add3b0e6db03af09e25a218bf1cad
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-03-17 13:34:33 +0100

    logs: avoiding postData logs on /api/users/:id/videos

--- server/app/middlewares/middleware-dumppostdata.js
+++ server/app/middlewares/middleware-dumppostdata.js
@@ -4,16 +4,19 @@ module.exports = function (options) {
   return function (req, res, next) {
     if (req && req.body) {
       try {
-        // fix: do not dump users passwords into logentries.
-        var body = JSON.parse(JSON.stringify(req.body));
-        if (body.password) { body.password = 'xxxxxxxx'; }
-        if (body.number) {
-          body.number = '4111111111111111';
-          if (body.year) body.year = 2018;
-          if (body.month) body.month = 1;
+        // avoiding to dump post on /api/users/.*/videos/
+        if (!String(req.url).match(/^\/api\/users\/\d+\/videos\/.*$/)) {
+          // fix: do not dump users passwords into logentries.
+          var body = JSON.parse(JSON.stringify(req.body));
+          if (body.password) { body.password = 'xxxxxxxx'; }
+          if (body.number) {
+            body.number = '4111111111111111';
+            if (body.year) body.year = 2018;
+            if (body.month) body.month = 1;
+          }
+          if (body.cvv) { body.cvv = '123'; }
+          console.log(req.url + ' postData =', body);
         }
-        if (body.cvv) { body.cvv = '123'; }
-        console.log(req.url + ' postData =', body);
       } catch (e) { }
     }
     next();
