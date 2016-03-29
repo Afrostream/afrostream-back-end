8c9b96dfce82d159a7272250ce83e5cba7baf9b1
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-03-29 14:40:05 +0200

    refacto: manual upsert, avoid heroku postgres log dump ([DATABASE] statement: CREATE OR REPLACE FUNCTION pg_temp.sequelize_upsert(...

--- server/app/api/user/video/video.controller.js
+++ server/app/api/user/video/video.controller.js
@@ -10,7 +10,18 @@ var UsersFavoritesEpisodes = sqldb.UsersFavoritesEpisodes;
 module.exports.update = function (req, res) {
   var userVideoKey = { userId: req.user._id, videoId: req.params.videoId};
   var data = _.merge({}, req.body, userVideoKey);
-  UsersVideos.upsert(data, { where: userVideoKey })
+  UsersVideos.findOne({where: userVideoKey})
+    .then(
+      function upsert(userVideo) {
+        // manual upsert, non atomic, but avoid heroku posgres log
+        // 2016-03-29T10:28:27Z app[postgres.24289]: [DATABASE] statement: CREATE OR REPLACE FUNCTION pg_temp.sequelize_upsert()...
+        if (!userVideo) {
+          return UsersVideos.create(data);
+        } else {
+          return userVideo.update(data);
+        }
+      }
+    )
     .then(
       function () { res.json({}); },
       function (err) { res.status(err.statusCode || 500).json({error: err.message || String(err)}); }
