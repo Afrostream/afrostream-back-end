a55bf941e9d053a02e17c7f8e192bbc0594d2979
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-04-03 10:44:11 +0200

    hotfix: 404 handler: should return 404 for GET /api/subscriptions but not for /subscriptions

--- server/app/routes.js
+++ server/app/routes.js
@@ -68,7 +68,7 @@ module.exports = function (app) {
     });
 
   // All other routes should redirect to the index.html
-  app.route(/\/(categorys|licensors|movies|seasons|episodes|videos|languages|images|users|plans|subscriptions|clients|actors|settings|login|logout|jobs|posts|catchup|users\-logs)/)
+  app.route(/^\/(categorys|licensors|movies|seasons|episodes|videos|languages|images|users|plans|subscriptions|clients|actors|settings|login|logout|jobs|posts|catchup|users\-logs)/)
     .get(function (req, res) {
       res.set('Cache-Control', 'public, max-age=0');
       res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
