0c390d8cae752dd4455690f323777f5ade7f3b6d
benjipott <pott.benjamin@gmail.com>
2016-04-13 11:45:25 +0200

    billing: add cashway provider

--- server/billing-api.js
+++ server/billing-api.js
@@ -153,7 +153,7 @@ var cancelSubscription = function (subscriptionBillingUuid) {
  */
 var getUser = function (userReferenceUuid, providerName) {
   assert(typeof userReferenceUuid === 'number' && userReferenceUuid);
-  assert(['gocardless', 'recurly', 'celery', 'bachat', 'afr'].indexOf(providerName) !== -1); // add other providers here later.
+  assert(['gocardless', 'recurly', 'celery', 'bachat', 'afr', 'cashway'].indexOf(providerName) !== -1); // add other providers here later.
 
   return requestBilling({
     url: config.billings.url + '/billings/api/users/'
