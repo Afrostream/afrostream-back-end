f9e678a96bc5c3784939c946f0c604111b68c83f
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-04-19 18:01:55 +0200

    bouygues: debug

--- server/app/api/exchange/bachat/index.js
+++ server/app/api/exchange/bachat/index.js
@@ -6,6 +6,12 @@ var auth = rootRequire('/server/auth/auth.service');
 
 var router = express.Router();
 
+// temporary debug
+router.use(function (req, res, next) {
+  console.log('HEADERS: ', req.headers);
+  next();
+});
+
 router.post('/customers', auth.isAuthenticated(), controller.customers);
 router.post('/subscriptions', auth.isAuthenticated(), controller.subscriptions);
 router.post('/refunds', auth.isAuthenticated(), controller.refunds);
