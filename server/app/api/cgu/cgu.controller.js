2eeab6ad9114215920842af05277528ed3e32750
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-04-26 12:20:23 +0200

    hotfix: wiztivi: legals & cgu are space free :) (hacky code..)

--- server/app/api/cgu/cgu.controller.js
+++ server/app/api/cgu/cgu.controller.js
@@ -4,5 +4,9 @@ var fs = require('fs');
 var cgu = fs.readFileSync(__dirname + '/cgu.html').toString();
 
 module.exports.index = function (req, res) {
+  // hack hack hack: preprocessing for wiztivi: removing tabs & \n
+  cgu = cgu.replace(/\r?\n|\t/gm, '').replace(/ +/gm, ' ');
+  cgu = cgu.replace(/>\s+</mg, '><');
+  //
   res.json({html:cgu});
 };
--- server/app/api/legals/legals.controller.js
+++ server/app/api/legals/legals.controller.js
@@ -4,5 +4,9 @@ var fs = require('fs');
 var legals = fs.readFileSync(__dirname + '/legals.html').toString();
 
 module.exports.index = function (req, res) {
+  // hack hack hack : preprocessing for wiztivi: removing tabs & \n
+  legals = legals.replace(/\r?\n|\t/gm, '').replace(/ +/gm, ' ');
+  legals = legals.replace(/>\s+</mg, '><');
+  //
   res.json({html:legals});
 };
