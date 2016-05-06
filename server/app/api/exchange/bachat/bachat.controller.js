80be43f857684d98333fd1478e28609c28ccff62
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-05-06 17:13:22 +0200

    bachat: logging input xml data

--- server/app/api/exchange/bachat/bachat.controller.js
+++ server/app/api/exchange/bachat/bachat.controller.js
@@ -1,13 +1,16 @@
 'use strict';
 
 module.exports.customers = function (req, res) {
+  console.log('[INFO]: [EXCHANGE]: [BACHAT]: customers = ' + req.body);
   res.send('');
 };
 
 module.exports.subscriptions = function (req, res) {
+  console.log('[INFO]: [EXCHANGE]: [BACHAT]: subscriptions = ' + req.body);
   res.send('');
 };
 
 module.exports.refunds = function (req, res) {
+  console.log('[INFO]: [EXCHANGE]: [BACHAT]: refunds = ' + req.body);
   res.send('');
 };
