fc96f3c4197a4d8d6ee5fb102a881e50e98c5081
benjipott <pott.benjamin@gmail.com>
2016-05-24 11:36:55 +0200

    fix cgu

--- server/app/api/policy/policy.controller.js
+++ server/app/api/policy/policy.controller.js
@@ -5,8 +5,8 @@ var policy = fs.readFileSync(__dirname + '/policy.html').toString();
 
 module.exports.index = function (req, res) {
   // hack hack hack: preprocessing for wiztivi: removing tabs & \n
-  policy = cgu.replace(/\r?\n|\t/gm, '').replace(/ +/gm, ' ');
-  policy = cgu.replace(/>\s+</mg, '><');
+  policy = policy.replace(/\r?\n|\t/gm, '').replace(/ +/gm, ' ');
+  policy = policy.replace(/>\s+</mg, '><');
   //
   res.json({html:policy});
 };
