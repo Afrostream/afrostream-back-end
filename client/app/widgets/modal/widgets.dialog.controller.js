ddc9f8880aee1f549848b743ed1b4b2cad5218c9
benjipott <pott.benjamin@gmail.com>
2016-07-07 13:29:01 +0200

    adjust widget

--- client/app/widgets/modal/widgets.dialog.controller.js
+++ client/app/widgets/modal/widgets.dialog.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('afrostreamAdminApp')
-  .controller('WidgetsDialogCtrl', function ($scope, $filter, $sce, $modal, Coupon) {
+  .controller('WidgetsDialogCtrl', function ($scope, $filter, $sce, $timeout, $modal, Coupon) {
 
     var generateTimeout = 0;
 
@@ -39,27 +39,30 @@ angular.module('afrostreamAdminApp')
       };
     };
 
-    $scope.$watch('item', function (item) {
-      clearTimeout(generateTimeout)
-      generateTimeout = setTimeout(function () {
-        var data = item && item.data || {};
-        data.class = data.class || 'afrostream-button';
-        data.width = data.width || 300;
-        data.height = data.height || 250;
-        $scope.code = '<script class="' + data.class + '" src="https://widget.afrostream.tv/dist/checkout.js"';
-        if (item && item.image && typeof item.image === 'object') {
-          $scope.code += 'data-src="' + item.image.imgix + '" data-width="' + data.width + '" data-height="' + data.height + '" ';
-        }
-        if (data.label) {
-          $scope.code += 'data-label="' + data.label + '" ';
-        }
-        if (data.coupon && typeof data.coupon === 'object') {
-          $scope.code += 'data-coupon="' + data.coupon.couponsCampaignBillingUuid + '" ';
-        }
-        $scope.code += '></script>'
-        $scope.jsonString = $sce.trustAsHtml($scope.code);
-      }, 1000)
+    $scope.$watch('item', function () {
+      generateTimeout = $timeout($scope.generateWidget, 1000)
     }, true);
+
+    $scope.generateWidget = function () {
+      var data = $scope.item && $scope.item.data || {};
+
+      data.class = data.class || 'afrostream-button';
+      data.width = data.width || 300;
+      data.height = data.height || 250;
+      $scope.code = '<script class="' + data.class + '" src="https://widget.afrostream.tv/dist/checkout.js"';
+      if ($scope.item && $scope.item.image && typeof $scope.item.image === 'object') {
+        $scope.code += 'data-src="' + $scope.item.image.imgix + '" data-width="' + data.width + '" data-height="' + data.height + '" ';
+      }
+      if (data.label) {
+        $scope.code += 'data-label="' + data.label + '" ';
+      }
+      if (data.coupon && typeof data.coupon === 'object') {
+        $scope.code += 'data-coupon="' + data.coupon.couponsCampaignBillingUuid + '" ';
+        data.label = data.label || data.coupon.name;
+      }
+      $scope.code += '></script>';
+      $scope.jsonString = $sce.trustAsHtml($scope.code);
+    }
 
     $scope.deliberatelyTrustDangerousSnippet = function () {
       return $sce.trustAsHtml($scope.code);
