cee113c26d2ffe1617e8461aa026f46d9ccca5ac
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-02 09:32:29 +0200

    bugfix: timeout on request because on error on middleware error

--- server/app/middlewares/middleware-error.js
+++ server/app/middlewares/middleware-error.js
@@ -1,10 +1,10 @@
 module.exports = function (options) {
   return function (req, res, next) {
-    req.handleError = function (res, statusCode) {
-      statusCode = statusCode || 500;
+    req.handleError = function (res, defaultStatusCode) {
+      defaultStatusCode = defaultStatusCode || 500;
       return function (err) {
         var message = err && err.message || err;
-        var statusCode = err && err.statusCode || statusCode;
+        var statusCode = err && err.statusCode || defaultStatusCode;
         console.error('[ERROR] ' + message, err);
         // all errors are "no-cache", prevent HW CDN cache on error
         res.noCache();
