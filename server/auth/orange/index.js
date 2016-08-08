4a20381f9ad5afa294c2bd8329ff173bbc5ede73
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-08 17:37:13 +0200

    logs dump passport

--- server/auth/orange/index.js
+++ server/auth/orange/index.js
@@ -11,11 +11,15 @@ router.get('/signin', orange.signin);
 router.get('/signup', orange.signup);
 router.post('/callback',
 function (req, res, next) {
-  console.log('[CALLBACK]: before passport preload')
+  console.log('[CALLBACK]: before passport preload');
+  console.log('[CALLBACK]: headers = ', req.headers);
   next();
 }, middlewarePassport({preload: true}),
 function (req, res, next) {
+  console.log('[CALLBACK]: passport.user', req.passport.user);
+  console.log('[CALLBACK]: passport.client', req.passport.client);
 
+  next();
 },
 orange.callback);
 router.get('/link', auth.isAuthenticated(), orange.link);
