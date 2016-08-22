794155fb6c7517c3f7deb28e997a85354a7d36fa
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-22 15:17:54 +0200

    middleware: country: default country is 'unknown' => if a content is filtered FR, and req.query.country is not given, we are 'unknown'

--- server/app/middlewares/middleware-country.js
+++ server/app/middlewares/middleware-country.js
@@ -4,7 +4,7 @@ var Country = rootRequire('/server/sqldb').Country;
 
 module.exports = function (options) {
   return function (req, res, next) {
-    Country.findOne({where: { _id: req.query.country || "FR" }})
+    Country.findOne({where: { _id: req.query.country || "--" }})
       .then(function (country) {
         if (!country) {
           throw new Error('no country ' + req.query.country);
