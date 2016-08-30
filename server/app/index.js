0e55104538890d45fba1db1ca872c7dd4658d9cd
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-30 11:00:50 +0200

    tempfix: bouygues use self signed certificates in production env...

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,9 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// tempfix: bouygues (faut les piquer)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 // global
 global.__basedir = __dirname + '/../..';
 global.rootRequire = function (name) { return require(global.__basedir + '/' + (name[0] === '/' ? name.substr(1) : name)); };
