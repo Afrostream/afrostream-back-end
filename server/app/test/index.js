295e688fd76cac84106fd9213c552a8a1e57bee4
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-09-01 16:41:56 +0200

    tests: /tests/{put,post,delete}?cached=true/false&...

--- server/app/test/index.js
+++ server/app/test/index.js
@@ -7,5 +7,8 @@ var router = express.Router();
 
 router.get('/log', controller.log);
 router.get('/mq', controller.mq);
+router.put('/put', controller.dumpHeaders);
+router.post('/post', controller.dumpHeaders);
+router.delete('/delete', controller.dumpHeaders);
 
 module.exports = router;
--- server/app/test/test.controller.js
+++ server/app/test/test.controller.js
@@ -13,3 +13,13 @@ module.exports.mq = function (req, res) {
   mq.send({date: new Date().toISOString(), q: req.query.q || 'foo'});
   res.send('done');
 };
+
+module.exports.dumpHeaders = function (req, res) {
+  if (req.query.cached) {
+    // HW may not have the same behavior on cached & not cached routes...
+    res.cache();
+  } else {
+    res.noCache();
+  }
+  res.json(req.headers);
+};
