fb2dc609a83312df4d470247ac8d2c9df2071880
benjipott <pott.benjamin@gmail.com>
2016-10-07 18:00:15 +0200

    life : add req.user

--- app/api/life/pin/index.js
+++ app/api/life/pin/index.js
@@ -6,7 +6,7 @@ var auth = rootRequire('/app/auth/auth.service');
 var utils = rootRequire('/app/api/utils.js');
 var router = express.Router();
 
-//router.use(auth.middleware.restrictRoutesToAuthentified());
+router.use(auth.middleware.restrictRoutesToAuthentified());
 
 router.get('/', utils.middlewareCache, controller.index);
 router.get('/:id', utils.middlewareCache, controller.show);
