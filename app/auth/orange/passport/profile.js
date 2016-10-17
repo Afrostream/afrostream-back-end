df5b8d4659e704e116b03460d35c99cad9461fbb
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-10-17 17:10:43 +0200

    doc: bump

--- app/auth/orange/passport/profile.js
+++ app/auth/orange/passport/profile.js
@@ -1,12 +1,5 @@
 var _ = require('lodash');
 
-/**
- * Parse profile.
- *
- * @param {Object|String} json
- * @return {Object}
- * @api private
- */
 exports.parse = function (json) {
   if ('string' == typeof json) {
     json = JSON.parse(json);
