c3d249ae6f2d45603abe60515f3180596bea2a0f
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-11-08 17:04:03 +0100

    bugfix: crons

--- cron/stats-usersVideos.js
+++ cron/stats-usersVideos.js
@@ -3,7 +3,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // global
-global.__basedir = __dirname + '/../..';
+global.__basedir = __dirname + '/..';
 global.rootRequire = function (name) { return require(global.__basedir + '/' + (name[0] === '/' ? name.substr(1) : name)); };
 
 var config = rootRequire('/config');
