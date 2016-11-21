9401d42cfca1f39cdaedd854a4118d9bcbd02912
benjipott <pott.benjamin@gmail.com>
2016-11-21 09:57:47 +0100

    fix life pins included model

--- app/api/life/pin/pin.includedModel.js
+++ app/api/life/pin/pin.includedModel.js
@@ -3,11 +3,41 @@
 var sqldb = rootRequire('/sqldb');
 var User = sqldb.User;
 var Image = sqldb.Image;
-var LifeThemePins = sqldb.LifeThemePins;
+var LifePin = sqldb.LifePin;
 
 module.exports.get = function () {
     return [
-        {model: LifeThemePins, all: true, required: true},
+        //{model: LifeThemePins, all: true},
+        {
+            model: LifePin,
+            as: 'pins',
+            attributes: [
+                '_id',
+                'type',
+                'title',
+                'role',
+                'providerUrl',
+                'providerName',
+                'originalUrl',
+                'imageUrl',
+                'richMediaUrl',
+                'description',
+                'date'],
+            required: true,
+            include: [
+                {model: Image, as: 'image', required: false},
+                {
+                    model: User,
+                    as: 'user',
+                    required: false
+                },
+                {
+                    model: User,
+                    as: 'users',
+                    required: false
+                }
+            ]
+        },
         {model: Image, as: 'image', required: false},
         {model: User, as: 'user', required: false},
         {model: User, as: 'users', required: false}
