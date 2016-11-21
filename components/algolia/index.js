12f71889c85d542ce1f3f65a19c52148ea306679
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-11-21 17:38:47 +0100

    bugfix: algolia

--- components/algolia/index.js
+++ components/algolia/index.js
@@ -1,10 +1,8 @@
-'use strict';
-var Promise = require('bluebird');
 var config = rootRequire('config');
 var algoliasearch = require('algoliasearch');
 var client = algoliasearch(config.algolia.appId, config.algolia.apiKey);
 
-var logger = rootRequire('logger').prefix('ALGOLIA');
+var Q = require('q');
 
 // FIXME: refactorer tout ca ...
 // les handlers d'error ne retournent rien ! (pas de fwd de l'erreur ??);
@@ -15,28 +13,19 @@ exports = module.exports = {
     return function (entitys) {
       if (entitys) {
         var index = client.initIndex(indexName);
-        //return Promise.map(entitys, function (entity) {
         // let's use table IDS as Algolia objectIDs
         var datas = entitys.map(function (result) {
           result.objectID = result._id;
           return result;
         });
-
-        var saveAsync = Promise.promisify(index.saveObjects, index);
-        return saveAsync(datas).then(function (items) {
-          return items;
-        }).catch(function (err) {
-          logger.error(err);
-        });
+        return Q.ninvoke(index, 'saveObjects', datas);
       }
     };
   },
 
   searchIndex: function (indexName, querystring) {
     var index = client.initIndex(indexName);
-
-    var searchAsync = Promise.promisify(index.search, index);
-    return searchAsync(querystring);
+    return Q.ninvoke(index, 'search', querystring);
   },
 
   handleError: function (res) {
