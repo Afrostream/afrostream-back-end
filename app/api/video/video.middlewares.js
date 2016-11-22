f6d030d1a9ef6c38658781a503d806cfbeb69516
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-11-22 15:21:33 +0100

    bugfix: missing video.middlewares.js

---
+++ app/api/video/video.middlewares.js
@@ -0,0 +1,10 @@
+const statsd = rootRequire('statsd');
+
+module.exports.metrics = function (req, res, next) {
+  if (req.passport &&
+      req.passport.client) {
+    const broadcasterId = statsd.escapeKey(req.passport.client.get('broadcasterId'));
+    statsd.client.increment('route.api.video.hit.broadcaster.'+broadcasterId);
+  }
+  statsd.middleware({route: 'api.video'})(req, res, next);
+};
