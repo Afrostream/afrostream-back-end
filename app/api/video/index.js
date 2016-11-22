4cd3a6549020e218aab4b52f0590a502616d477b
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-11-22 15:17:28 +0100

    metrics: video hit by broadcaster

--- app/api/video/index.js
+++ app/api/video/index.js
@@ -29,13 +29,10 @@ const controller = require('./video.controller.js');
 const auth = rootRequire('app/auth/auth.service');
 const utils = rootRequire('app/api/utils.js');
 const router = express.Router();
-const middlewareStatsd = rootRequire('statsd').middleware;
+const middlewares = require('./video.middlewares.js');
 
 // all video routes cannot be cached.
-router.use((req, res, next) => {
-  res.noCache();
-  next();
-});
+router.use(utils.middlewareNoCache);
 
 router.use(auth.middleware.restrictRoutesToAuthentified());
 
@@ -43,14 +40,14 @@ router.use(auth.middleware.restrictRoutesToAuthentified());
 router.use('/:videoId/comments', require('./comment'));
 
 // import
-router.get('/importFromPfContent', utils.middlewareNoCache, auth.hasRole('admin'), controller.importFromPfContent);
+router.get('/importFromPfContent', auth.hasRole('admin'), controller.importFromPfContent);
 
 // video manipulation.
-router.get('/', utils.middlewareNoCache, auth.hasRole('admin'), controller.index);
-router.get('/:id', middlewareStatsd({route: 'api.video'}), controller.show);
-router.post('/', utils.middlewareNoCache, auth.hasRole('admin'), controller.create);
-router.put('/:id', utils.middlewareNoCache, auth.hasRole('admin'), controller.update);
-router.patch('/:id', utils.middlewareNoCache, auth.hasRole('admin'), controller.update);
-router.delete('/:id', utils.middlewareNoCache, auth.hasRole('admin'), controller.destroy);
+router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/:id', middlewares.metrics(), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
