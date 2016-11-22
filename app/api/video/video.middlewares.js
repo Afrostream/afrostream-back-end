4f72d537351536540f88c9832c603feac8235c3b
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-11-22 15:31:30 +0100

    bugfix²: req.passport is not defined...

--- app/api/video/video.middlewares.js
+++ app/api/video/video.middlewares.js
@@ -1,10 +1,12 @@
 const statsd = rootRequire('statsd');
 
-module.exports.metrics = function (req, res, next) {
-  if (req.passport &&
-      req.passport.client) {
-    const broadcasterId = statsd.escapeKey(req.passport.client.get('broadcasterId'));
-    statsd.client.increment('route.api.video.hit.broadcaster.'+broadcasterId);
-  }
-  statsd.middleware({route: 'api.video'})(req, res, next);
+module.exports.metrics = function (options) {
+  return function (req, res, next) {
+    if (req.passport &&
+        req.passport.client) {
+      const broadcasterId = statsd.escapeKey(req.passport.client.get('broadcasterId'));
+      statsd.client.increment('route.api.video.hit.broadcaster.'+broadcasterId);
+    }
+    statsd.middleware({route: 'api.video'})(req, res, next);
+  };
 };
