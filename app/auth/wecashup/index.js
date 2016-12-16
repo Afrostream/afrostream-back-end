98d956a9706fdf31d424a8735fcf5f032f88a365
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-12-16 17:47:22 +0100

    wecashup: routes are no-cache

--- app/auth/wecashup/index.js
+++ app/auth/wecashup/index.js
@@ -5,6 +5,11 @@ var auth = require('../auth.service');
 var controller = require('./wecashup.controller.js');
 var router = express.Router();
 
+router.use((req, res, next) => {
+  res.noCache();
+  next();
+});
+
 router.get('/check', auth.middleware.restrictRoutesToAuthentified(), controller.check);
 router.post('/callback', controller.callback);
 
