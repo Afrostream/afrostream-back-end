4ee6a7df58f36912a6fc2302ee3ac4b0be9615bc
benjipott <pott.benjamin@gmail.com>
2016-12-20 17:45:53 +0100

    try fix passport

--- app/auth/oauth2/passport.js
+++ app/auth/oauth2/passport.js
@@ -37,7 +37,7 @@ function localAuthenticate (User, email, password, done) {
 }
 
 function clientAuthenticate (Client, clientId, clientSecret, done) {
-  return Client.find({
+  Client.find({
     where: {
       _id: clientId
     }
