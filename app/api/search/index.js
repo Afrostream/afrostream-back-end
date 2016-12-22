02b9b6491c4c93e6ee5172ab062837fa5821fbbd
benjipott <pott.benjamin@gmail.com>
2016-12-22 16:00:58 +0100

    multi search route

--- app/api/search/index.js
+++ app/api/search/index.js
@@ -1,11 +1,11 @@
 'use strict';
 const express = require('express');
 const controller = require('./search.controller.js');
-//const auth = rootRequire('app/auth/auth.service');
+const auth = rootRequire('app/auth/auth.service');
 const utils = rootRequire('app/api/utils.js');
 const router = express.Router();
 
-//router.use(auth.middleware.restrictRoutesToAuthentified());
+router.use(auth.middleware.restrictRoutesToAuthentified());
 
 router.post('/', utils.middlewareCache, controller.search);
 
