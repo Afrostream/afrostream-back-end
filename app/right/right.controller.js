b038f2490f83d8a0aa445d1e97234be10e3a7082
benjipott <pott.benjamin@gmail.com>
2016-12-27 16:28:39 +0100

    fix eslint

--- app/right/right.controller.js
+++ app/right/right.controller.js
@@ -2,9 +2,9 @@
 
 var Video = rootRequire('sqldb').Video;
 
-//var authenticate = require('../auth/auth.service').authenticate;
+var authenticate = require('../auth/auth.service').authenticate;
 
-module.exports.drmtodayCallback = function (req, res/*, next*/) {
+module.exports.drmtodayCallback = function (req, res, next) {
   var accessToken = req.query.sessionId;
   var userId = req.params.userId;
   var encodingId = req.params.assetId;
@@ -17,53 +17,53 @@ module.exports.drmtodayCallback = function (req, res/*, next*/) {
   res.set('Content-Type', 'application/json');
 
   /*return Video.find({where: {encodingId: encodingId}}).then(function (video) {
-    if (!video) throw 'unknown asset '+encodingId;
-    return video;
-  }).then(
-    function (video) { return video; },
-    function () { return { _id: 'unknown', name: 'unknown' }; }
-  ).then(function (video) {
-    res.json({
-      "accountingId": userId + ":" + video._id + ":" + video.name,
-      "profile": {
-        "rental": {
-          "absoluteExpiration": new Date(new Date().getTime() + 1000 * 3600 * 24).toISOString(), // 1 day
-          "playDuration": 1000 * 3600 * 12 // 12 hours
-        }
-      },
-      "message": "granted"
-    });
-  });*/
+   if (!video) throw 'unknown asset '+encodingId;
+   return video;
+   }).then(
+   function (video) { return video; },
+   function () { return { _id: 'unknown', name: 'unknown' }; }
+   ).then(function (video) {
+   res.json({
+   "accountingId": userId + ":" + video._id + ":" + video.name,
+   "profile": {
+   "rental": {
+   "absoluteExpiration": new Date(new Date().getTime() + 1000 * 3600 * 24).toISOString(), // 1 day
+   "playDuration": 1000 * 3600 * 12 // 12 hours
+   }
+   },
+   "message": "granted"
+   });
+   });*/
   /*
-  //
-  // hack, test orange, to be removed !
-  // BEGIN HACK
-  if (userId === '12345') {
-    return Video.find({where: {encodingId: encodingId}}).then(function (video) {
-      if (!video) throw 'unknown asset '+encodingId;
-      return video;
-    }).then(
-      function (video) { return video; },
-      function () { return { _id: 'unknown', name: 'unknown' }; }
-    ).then(function (video) {
-      res.json({
-        "accountingId": userId + ":" + video._id + ":" + video.name,
-        "profile": {
-          "rental": {
-            "absoluteExpiration": new Date(new Date().getTime() + 1000 * 3600 * 24).toISOString(), // 1 day
-            "playDuration": 1000 * 3600 * 12 // 12 hours
-          }
-        },
-        "message": "granted"
-      });
-    });
-  }
-  */
+   //
+   // hack, test orange, to be removed !
+   // BEGIN HACK
+   if (userId === '12345') {
+   return Video.find({where: {encodingId: encodingId}}).then(function (video) {
+   if (!video) throw 'unknown asset '+encodingId;
+   return video;
+   }).then(
+   function (video) { return video; },
+   function () { return { _id: 'unknown', name: 'unknown' }; }
+   ).then(function (video) {
+   res.json({
+   "accountingId": userId + ":" + video._id + ":" + video.name,
+   "profile": {
+   "rental": {
+   "absoluteExpiration": new Date(new Date().getTime() + 1000 * 3600 * 24).toISOString(), // 1 day
+   "playDuration": 1000 * 3600 * 12 // 12 hours
+   }
+   },
+   "message": "granted"
+   });
+   });
+   }
+   */
   // END HACK
 
   // we check if the user exist & if the accessToken is valid.
   authenticate(req, res, next)
-    .then(function checkUser(data) {
+    .then(function checkUser (data) {
       var user = data[0]; // info = data[1];
       // unknown user => break
       if (!user) {
@@ -73,27 +73,27 @@ module.exports.drmtodayCallback = function (req, res/*, next*/) {
         throw 'userId mismatch userTokenId';
       }
     })
-    .then(function checkEncodingId() {
+    .then(function checkEncodingId () {
       return Video.find({where: {encodingId: encodingId}}).then(function (video) {
-        if (!video) throw 'unknown encodingId '+encodingId;
+        if (!video) throw 'unknown encodingId ' + encodingId;
         return video;
       });
     })
     .then(
-      function success(video) {
+      function success (video) {
         req.logger.log('DRM: ' + req.originalUrl + ' granted !');
         res.json({
           "accountingId": userId + ":" + video._id + ":" + video.name,
           "profile": {
-            "rental" : {
-              "absoluteExpiration" : new Date(new Date().getTime() + 1000 * 3600* 24).toISOString(), // 1 day
-              "playDuration" : 1000 * 3600 * 12 // 12 hours
+            "rental": {
+              "absoluteExpiration": new Date(new Date().getTime() + 1000 * 3600 * 24).toISOString(), // 1 day
+              "playDuration": 1000 * 3600 * 12 // 12 hours
             }
           },
-          "message":"granted"
+          "message": "granted"
         });
       },
-      function error(err) {
+      function error (err) {
         req.logger.error('DRM: ' + req.originalUrl + ' not granted with the error ', err.message);
         res.json({
           message: 'not granted',
