51123cf9da89807656b6d46bda611a1682c3bff8
benjipott <pott.benjamin@gmail.com>
2017-02-15 17:03:33 +0100

    fix followers unique key

--- sqldb/models/life/lifeUsersFollowers.js
+++ sqldb/models/life/lifeUsersFollowers.js
@@ -11,7 +11,7 @@ module.exports = function (sequelize, DataTypes) {
     },
     userId: {
       type: DataTypes.INTEGER,
-      unique: 'userId_userId'
+      unique: 'userId_followedUserId'
     },
     followUserId: {
       type: DataTypes.INTEGER,
