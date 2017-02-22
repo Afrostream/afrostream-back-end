702886db224ba4f67bbb3ebe89b476656220a034
benjipott <pott.benjamin@gmail.com>
2017-02-22 11:34:10 +0100

    fiw where group clause

--- app/api/life/user/lifeUser.includedModel.js
+++ app/api/life/user/lifeUser.includedModel.js
@@ -2,19 +2,10 @@
 
 const sqldb = rootRequire('sqldb');
 const LifePin = sqldb.LifePin;
-const Image = sqldb.Image;
 
 module.exports.get = () => [{
   model: LifePin,
   as: 'lifePins',
   required: false,
-  attributes: [],
-  include: [{
-    model: Image,
-    as: 'image',
-    group: [
-      ['_id']
-    ],
-    required: false
-  }]
+  attributes: []
 }];
