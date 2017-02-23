2ade6a28b9e795c8d802e92071e717b7782eabf6
benjipott <pott.benjamin@gmail.com>
2017-02-23 14:52:42 +0100

    algolia : import all staging|prod indices

--- components/algolia/index.js
+++ components/algolia/index.js
@@ -12,7 +12,7 @@ exports = module.exports = {
   importAll: function (res, indexName) {
     return function (entitys) {
       if (entitys) {
-        var index = client.initIndex(indexName);
+        var index = client.initIndex(process.env.NODE_ENV + '_' + indexName);
         // let's use table IDS as Algolia objectIDs
         var datas = entitys.map(function (result) {
           result.objectID = result._id;
@@ -24,6 +24,8 @@ exports = module.exports = {
   },
 
   searchIndex: function (indexName, querystring) {
+    //FIXME when export Algolia en prod, replace this line
+    // var index = client.initIndex(process.env.NODE_ENV + '_' + indexName);
     var index = client.initIndex(indexName);
     return Q.ninvoke(index, 'search', querystring);
   },
