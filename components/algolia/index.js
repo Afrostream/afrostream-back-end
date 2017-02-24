e58fd6776f667b3ff797f5379ead14d9d1abbbaa
benjipott <pott.benjamin@gmail.com>
2017-02-24 15:06:42 +0100

    import new index

--- components/algolia/index.js
+++ components/algolia/index.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var config = rootRequire('config');
 var algoliasearch = require('algoliasearch');
 var client = algoliasearch(config.algolia.appId, config.algolia.apiKey);
@@ -11,10 +12,31 @@ var Q = require('q');
 exports = module.exports = {
   importAll: function (res, indexName) {
     return function (entitys) {
+
+      const removeEmptyObjects = function (obj) {
+        return function prune (current) {
+          _.forOwn(current, function (value, key) {
+            if (_.isUndefined(value) || _.isNull(value) || _.isNaN(value) ||
+              (_.isString(value) && _.isEmpty(value)) ||
+              (_.isArray(value)) ||
+              (_.isObject(value) && _.isEmpty(prune(value)))) {
+
+              delete current[key];
+            }
+          });
+          // remove any leftover undefined values from the delete
+          // operation on an array
+          if (_.isArray(current)) _.pull(current, undefined);
+
+          return current;
+
+        }(_.cloneDeep(obj));  // Do not modify the original object, create a clone instead
+      };
       if (entitys) {
         var index = client.initIndex(process.env.NODE_ENV + '_' + indexName);
         // let's use table IDS as Algolia objectIDs
         var datas = entitys.map(function (result) {
+          result = removeEmptyObjects(result);
           result.objectID = result._id;
           return result;
         });
