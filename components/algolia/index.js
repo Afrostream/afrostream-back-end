a8606907a8bedec86550be84b6d97396d9cfd9d1
benjipott <pott.benjamin@gmail.com>
2017-02-24 15:50:12 +0100

    fix import algolia new indices

--- components/algolia/index.js
+++ components/algolia/index.js
@@ -37,7 +37,7 @@ exports = module.exports = {
         var index = client.initIndex(process.env.NODE_ENV + '_' + indexName);
         // let's use table IDS as Algolia objectIDs
         var datas = entitys.map(function (result) {
-          var item = removeEmptyObjects(result.dataValues);
+          var item = removeEmptyObjects(result.toJSON());
           item.objectID = result._id;
           return item;
         });
