20f4d10cb19becf589aedf26183a3722eaa26547
benjipott <pott.benjamin@gmail.com>
2017-02-27 18:03:34 +0100

    fix lint

--- app/auth/facebook/facebook.controller.js
+++ app/auth/facebook/facebook.controller.js
@@ -135,56 +135,6 @@ var callback = function (req, res, next) {
   })(req, res, next);
 };
 
-var facebookToken = function (req, res, next) {
-  var logger = req.logger.prefix('AUTH').prefix('FACEBOOK').prefix('MOBILE SDK');
-  logger.log('start');
-  passport.authenticate('facebook-token', {},
-    function (err, user, info) {
-      if (err) {
-        logger.log('authenticate done, error ' + err.message, JSON.stringify(err));
-      } else {
-        logger.log('authenticate done, no error, info = ' + JSON.stringify(info));
-      }
-      Q()
-        .then(function () {
-          if (err) throw err;
-          //if (info) throw info;
-          if (!user) throw new Error('Something went wrong, please try again.');
-          logger.log('authenticate getOauth2UserTokens', user._id);
-          return req.getPassport();
-        })
-        .then(function (passport) {
-          logger.log('generate token with client', passport.client._id, user._id);
-          var deferred = Q.defer();
-          oauth2.generateToken({
-            client: passport.client,
-            user: user,
-            code: null,
-            userIp: req.clientIp,
-            userAgent: req.userAgent,
-            expireIn: null
-          }, function (err, accessToken, refreshToken, info) {
-            logger.log('token generated ');
-            if (err) return deferred.reject(err);
-            return deferred.resolve({
-              token: accessToken,
-              access_token: accessToken,
-              refresh_token: refreshToken,
-              expires_in: info.expires_in
-            });
-          });
-          return deferred.promise;
-        })
-        .then(
-          function success (tokens) {
-            logger.log('sending tokens ' + JSON.stringify(tokens));
-            res.json(tokens);
-          },
-          res.handleError()
-        );
-    })(req, res, next);
-};
-
 module.exports.middlewares = {
   strategyOptions: strategyOptions
 };
