1f790ddcc23aaf4c4c83de2966bc599bc632fcb3
benjipott <pott.benjamin@gmail.com>
2017-03-08 15:37:54 +0100

    replace auth on app/config route

--- app/api/app/index.js
+++ app/api/app/index.js
@@ -6,6 +6,8 @@ const auth = rootRequire('app/auth/auth.service');
 const utils = rootRequire('app/api/utils.js');
 const router = express.Router();
 
+router.use(auth.middleware.restrictRoutesToAuthentified());
+
 router.get('/config', utils.middlewareCache, controller.showConfig);
 
 module.exports = router;
