301c1f2105ff00f09bd883538caf7098e3c2ffef
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-06-06 21:38:40 +0200

    mailer: MailerProvider: should be able to load by id.

--- mailer/MailerProvider.js
+++ mailer/MailerProvider.js
@@ -1,16 +1,49 @@
+const assert = require('better-assert');
+
 const sqldb = rootRequire('sqldb');
+
+const Mailblast = require('./APIInterface/Mailblast.js');
 
 class MailerProvider {
   constructor () {
+    this.apiInterface = null;
+  }
 
+  loadById(id) {
+    return sqldb.MailerProvider.find({
+      where: { _id: id }
+    })
+    .then(model => {
+      if (!model) throw new Error('cannot read from db');
+
+      this.model = model;
+      return this;
+    });
   }
 
   getAPIInterface() {
-    
+    if (!this.apiInterface) {
+      const interfaceName = this.model.get('interface');
+
+      switch (interfaceName) {
+        case 'Mailblast':
+          this.apiInterface = new Mailblast();
+          break;
+        default:
+          throw new Error(`unknown apiInterface ${interfaceName}`);
+      }
+    }
+    return this.apiInterface;
   }
 }
 
 MailerProvider.getDbModel = () => sqldb.MailerProvider;
 
+MailerProvider.loadById = id => {
+  assert(id);
+
+  const mailerProvider = new MailerProvider();
+  return mailerProvider.loadById(id);
+};
 
 module.exports = MailerProvider;
