550e43e48bb69b6b8f8dd3eef9f9407a42386cd9
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-06-06 16:02:21 +0200

    mailer: API Interface: AfrostreamMailer <-> [API Interface] <-> Provider (ex: maiblast)

---
+++ mailer/APIInterface/APIInterface.js
@@ -0,0 +1,75 @@
+const assert = require('better-assert');
+
+const MailerProvider = require('../MailerProvider');
+
+class APIInterface {
+  constructor (mailerProvider) {
+    assert(mailerProvider instanceof MailerProvider);
+
+    this.mailerProvider = mailerProvider;
+  }
+
+  canHandleList() { return false; }
+
+  // disaling lint for interface definition.
+  /*eslint-disable*/
+  createList(mailerList) { assert(false); }
+  removeList(id) { assert(false); }
+  updateList(data) { assert(false); }
+  hasList(id) { assert(false); }
+  /*eslint-enable*/
+}
+
+class List {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.subscribers = [];
+  }
+
+  load(data) {
+    assert(data);
+    assert(data.id && typeof data.id === 'string');
+    assert(data.name && typeof data.name === 'string');
+
+    this.id = data.id;
+    this.name = data.name;
+    if (Array.isArray(data.subscribers)) {
+      this.subscribers = [];
+    }
+  }
+
+  set(key, val) {
+    this.data[key] = val;
+  }
+
+  get(key) {
+    return this.data[key];
+  }
+}
+
+// check if some data can be an provider interface list
+List.isIList = data => {
+  return data &&
+    data.id &&
+    typeof data.id === 'string' &&
+    data.attributes &&
+    typeof data.attributes.name === 'string' &&
+    data.attributes.name;
+};
+
+List.build = data => {
+  if (List.isIList(data)) {
+    throw new Error('data is not an IList ' + JSON.stringify(data));
+  }
+  return new List(data);
+};
+
+class Subscriber {
+
+}
+
+APIInterface.List = List;
+APIInterface.Subscriber = Subscriber;
+
+module.exports = APIInterface;
---
+++ mailer/APIInterface/Mailblast.js
@@ -0,0 +1,155 @@
+const assert = require('better-assert');
+const Q = require('q');
+
+const ApiInterface = require('./interface');
+const IList = ApiInterface.List;
+
+const Mailer = require('../Mailer');
+const MailerList = Mailer.List;
+
+const request = require('request');
+
+// fixme: this dependency should be injected
+const logger = rootRequire('logger').prefix('MAILER').prefix('MAILBLAST');
+
+const requestMailblast = options => {
+  assert(options);
+  assert(options.uri);
+
+  // fixme: move to config
+  const queryOptions = Object.assign({}, {
+    headers: {
+      'X-USER-TOKEN': 'y5MS3F4vq9X-g7o328vrZHszYxueSA',
+      'X-USER-EMAIL': 'tech@afrostream.tv'
+    },
+    json: true
+  }, options);
+
+  logger.log('=> '+JSON.stringify(queryOptions));
+
+  return Q.nfcall(request, queryOptions).then(
+    // log + fwd
+    d => {
+      try {
+        if (d && d[0] && d[0].statusCode) {
+          logger.log(`<= >${d[0].statusCode}< ${JSON.stringify(d[1])}`);
+        } else {
+          logger.log(`<= unknown statusCode , ${JSON.stringify(d)}`);
+        }
+      } catch (e) {
+        logger.log(`<= unknown (err=${e.message})`);
+      }
+      return d;
+    },
+    err => {
+      logger.error(err.message);
+      throw err;
+    }
+  );
+};
+
+/*
+ * convert provider list to interface list, throw error if error.
+ *
+ pList
+ {
+  "id": "[ID]",
+  "type": "lists",
+  "attributes": {
+    "name": "List 1",
+    "active_count": 0,
+    "created_at": "2017-01-01T00:00:00Z"
+  }
+
+ iList
+ {
+  "id": "...",
+  "name": "..."
+}
+ */
+
+class Mailblast extends ApiInterface {
+  constructor(options) {
+    assert(options);
+    assert(options.mailerProvider);
+
+    super(options.mailerProvider);
+  }
+  // this provider can handle lists.
+  canHandleList() { return true; }
+
+  /*
+   * @param mailerList  MailerList
+   * @return IList
+   */
+  createList(mailerList) {
+    assert(mailerList instanceof MailerList);
+
+    logger.log(`creating list ${mailerList.name}`);
+
+    return requestMailblast({
+      method: 'POST',
+      uri: 'https://api.mailblast.io/v1/lists',
+      body: {
+        data: {
+          attributes: { name: mailerList.name }
+        }
+      }
+    })
+      .then(([response, body]) => {
+        if (!response.statusCode !== 201) throw new Error('http status should be 201');
+        if (!body || !body.data) throw new Error('missing response.data');
+        if (!Mailblast.isPList) throw new Error('malformated response');
+        // everything seems ok in mailblast system.
+        const iList = Mailblast.PListToIList(body.data);
+        logger.log(`iList created from ${JSON.stringify(body)}`);
+        return iList;
+      });
+  }
+
+  removeList(id) {
+    assert(id && typeof id === 'string');
+
+    return requestMailblast({
+      method: 'DELETE',
+      uri: `https://api.mailblast.io/v1/lists/${id}`
+    })
+    .then(([response]) => {
+      if (!response.statusCode !== 204) throw new Error('http status should be 204');
+      return true;
+    });
+  }
+}
+
+// convertion functions
+Mailblast.PListToIList = pList => {
+  assert(pList);
+
+  return IList.build({
+    id: pList && pList.id || null,
+    name: pList && pList.attributes && pList.attributes.name || null
+  });
+};
+
+Mailblast.IListToPList = iList => {
+  assert(iList);
+
+  return {
+    id: iList && iList.id || null,
+    attributes: {
+      name: iList && iList.name || null
+    }
+  };
+};
+
+// existance functions
+Mailblast.isPList = pList => {
+  return pList &&
+    pList.id &&
+    typeof pList.id === 'string' &&
+    pList.attributes &&
+    pList.attributes.name &&
+    typeof pList.attributes.name === 'string';
+};
+
+module.exports = Mailblast;
