4a89f722956ecd2e92b9ecdb5a94ddad66c041ab
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-06-23 15:18:35 +0200

    cron: mailer (foreach list : basic list.runQuery() & list.sync(provider) foreach providers)

---
+++ cron/mailer.js
@@ -0,0 +1,56 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// global
+global.__basedir = __dirname + '/..';
+global.rootRequire = function (name) { return require(global.__basedir + '/' + (name[0] === '/' ? name.substr(1) : name)); };
+
+const sqldb = rootRequire('sqldb');
+const Q = require('q');
+
+const logger = rootRequire('logger').prefix('CRON').prefix('MAILER');
+
+const Mailer = rootRequire('mailer');
+
+logger.log('start');
+
+sqldb.MailerList.findAll({
+  where: {
+    disabled: false,
+    cron: true
+  }
+}).then(lists => {
+  return Q.all(lists.map(list=>Mailer.List.loadFromDB(list)));
+}).then(mailerLists => {
+  mailerLists.forEach(mailerList =>
+    logger.log(mailerList.getName() + ' -> runQuery() ')
+  );
+  // first: we run the query
+  /* disabled (temp)
+  return mailerLists.reduce((p, c) => p.then(() => c.runQuery()), Q());
+  */
+  return mailerLists;
+}).then(mailerLists => {
+  mailerLists.forEach(mailerList =>
+    logger.log(mailerList.getName() + ' -> getProviders() -> startSync()')
+  );
+  /* disabled (temp)
+  // second: we sync the providers
+  return mailerLists.reduce((p, mailerList) => {
+    return p.then(() => {
+      // for each mailerList, we search the provider
+      return mailerList.getProviders()
+        .then(mailerProviders => Q.all(
+          mailerProviders.map(mailerProvider => mailerList.startSync(mailerProvider))
+        ));
+    });
+  }, Q());
+  */
+}).then(() => {
+  logger.log('success');
+  process.exit();
+},
+e => {
+  logger.error(e.message);
+  process.exit();
+}
+);
